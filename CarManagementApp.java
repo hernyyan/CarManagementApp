c353d32f1dd76025b7c75629f31bb11568c243bd
sksbn03 <ks6833@nyu.edu>
2024-11-18 14:28:50 -0500

    Update CarManagementApp.java

--- CarManagementApp.java
+++ CarManagementApp.java
@@ -4,35 +4,50 @@ import java.text.DecimalFormat;
 public class CarManagementApp {
 
 	public static void main(String[] args) {
-		
+		//set scanner
 		Scanner sc = new Scanner(System.in);
-		
+
+		//create array of cars
 		Car[] carList = new Car[10];
+
+		//variable for user choice
 		int c = 0;
+
 		
 		while (c != 9) {
-			
+
+			//call menu method
 			c = menu();
-			
+
+			//add car
 			if (c == 1) {
 				addCar(carList);
+			//edit car
 			} else if (c == 2) {
 				editCar(carList);
+			//set specific car to fixed
 			} else if (c == 3) {
 				setFixed(carList);
+			//display all cars
 			} else if (c == 4) {
 				displayCars(carList);
+			//delete a car using id
 			} else if (c == 5) {
 				deleteCar(carList);
+			//display cars with service cost > certain price 
 			} else if (c == 6) {
 				expensiveFixes(carList);
+			//set specific car to not fixed 
 			} else if (c == 7){
 				setNotFixed(carList);
+			//display profit from fixing the cars
 			} else if (c == 8) {
 				displayProfit(carList);
+			//end program
 			} else if (c == 9){
 				System.out.println("Goodbye");
 				break;
+			//invalid input
 			} else {
 				System.out.println("Invalid choice");
 			}
@@ -40,6 +55,7 @@ public class CarManagementApp {
 		sc.close();
 	}
 
+	//method for the menu
 	public static int menu() {
 
 		Scanner sc = new Scanner(System.in);
@@ -63,7 +79,8 @@ public class CarManagementApp {
 		sc.nextLine();
 		return choice;	
 	}
-	
+
+	//method to add car 
 	public static void addCar(Car[] carList) {
 		
 		Scanner sc = new Scanner(System.in);
@@ -78,11 +95,12 @@ public class CarManagementApp {
 				full = false;
 			}
 		}
-		
+		//when system is not full
 		if (!full) {
+			//name of car
 			System.out.println("Enter new car name: ");
 			newname = sc.nextLine();
-			
+			//car id
 			do {
 				
 				System.out.println("Enter new car ID: ");
@@ -99,13 +117,13 @@ public class CarManagementApp {
 					}
 				}
 			} while(repeat);
-			
+			//fixed status 
 			System.out.println("Enter 1 if car is fixed, 0 if not fixed");
 			if (sc.nextInt() == 1) {
 				newstatus = true;
 				sc.nextLine();
 			}
-			
+			//service fee
 			System.out.println("Enter new car service fee: ");
 			newfee = sc.nextDouble();
 			sc.nextLine();
@@ -118,12 +136,13 @@ public class CarManagementApp {
 					break;
 				}
 			}
-	
+		//when system is full
 		} else {
 			System.out.println("System is full, delete a car first");
 		}
 	}
-	
+
+	//method to edit car
 	public static void editCar(Car[] carList) {
 		
 		Scanner sc = new Scanner(System.in);
@@ -134,7 +153,8 @@ public class CarManagementApp {
 		double editfee = 0;
 		boolean found = false;
 		boolean repeat = false;
-		
+
+		//getting car ID -- repeat if id is not found
 		while (!found) {
 			System.out.println("Enter car ID: ");
 			editcarid = sc.nextInt();
@@ -150,7 +170,8 @@ public class CarManagementApp {
 				System.out.println("Not found, enter again");
 			}
 		}
-		
+
+		//if id exists
 		if (found) {
 			System.out.println("The selected car:");
 			editcar.printme();
@@ -162,11 +183,12 @@ public class CarManagementApp {
 			editparam = sc.nextInt();
 			sc.nextLine();
 		}
-		
+		//editing car name
 		if (editparam == 1) {
 			System.out.println("Enter the new car name: ");
 			editname = sc.nextLine();
 			editcar.setName(editname);
+		//editing car id
 		} else if (editparam == 2) {
 			do {
 							
@@ -186,16 +208,18 @@ public class CarManagementApp {
 			} while(repeat);
 			
 			editcar.setId(editid);
+		//editing service fee
 		} else if (editparam == 3) {
 			editfee = sc.nextDouble();
 			sc.nextLine();
 			editcar.setFee(editfee);
 		}
-		
+		//print updated version
 		System.out.println("Updated car details:");
 		editcar.printme();
 	}
 
+	//method to set specific car to fixed
 	public static void setFixed(Car[] carList) {
 		  // initiliaze scanner
 		  Scanner sc = new Scanner(System.in);
@@ -240,6 +264,7 @@ public class CarManagementApp {
 		  }
 	}
 
+	//method to set specific car to not fixed
 	public static void setNotFixed(Car[] carList) {
 		  // same logic as setFixed method
 		  Scanner sc = new Scanner(System.in);
@@ -276,23 +301,27 @@ public class CarManagementApp {
 		  }
 	}
 
+	//method to display all cars in array 
 	public static void displayCars (Car [] myInventory) {
 	    for (int i = 0; i < myInventory.length; i++) {
 	        Car currentCar = myInventory[i];
 	        if (currentCar != null) {
+		//use printme in the car class
 	            currentCar.printme();
 	        }
 	    }
 	}
-	
+
+	//method to delete a car
 	public static void deleteCar (Car [] carList) {
 	    int input;
 	    boolean deleteStatus = false;
 	    Scanner sc = new Scanner (System.in);
-	    
+
+	//enter id
 	    System.out.println("Enter the ID of the car you wish to delete: ");
 	    input = sc.nextInt();
-	    
+		//if car exists in the system change it to null
 	    for (int i = 0; i < carList.length; i++) {
 	        Car currentCar = carList[i];
 	        if (currentCar != null && currentCar.getId() == input) {
@@ -302,27 +331,32 @@ public class CarManagementApp {
 	            break;
 	        }
 	    }
+		//if car does not exist in the system
 	    if (deleteStatus == false) {
 	        System.out.println("A car with the entered ID does not exist in the system.");
 	    }
 	}
-	
+
+	//method to display cars with service fees that exceed specified amount
 	public static void expensiveFixes(Car[] carList) {
 
 	    Scanner sc = new Scanner(System.in);
-	    
+
+	//get specified price
 	    System.out.println("Enter specified price: ");
 	    double price = sc.nextDouble();
-	    
+	    //print result if price exceeds the specified price
 	    for (int i = 0; i < carList.length; i++) {
 	    	if (carList[i] != null && carList[i].getFee() > price) {
 	    		carList[i].printme();
 	    	}
 	    }
 	}
- 
+
+	//method to display profit from fixing cars
 	public static void displayProfit(Car[] carList) {
-	
+
+		//set decimal format because it is USD 
 		DecimalFormat twodp = new DecimalFormat("#.##");
 		
 		double total = 0;
